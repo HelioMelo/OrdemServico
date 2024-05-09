d7eda5c57da90cbbca235f12e653d844d1024e7c
Helio Melo <helio_melo_@hotmail.com>
2024-05-09 16:35:09 -0300

    add: pastas de arquivo

---
+++ servico/src/main/java/br/com/servico/domain/technician/technician.java
@@ -0,0 +1,48 @@
+package br.com.servico.domain.technician;
+
+import javax.xml.crypto.Data;
+
+
+
+public class technician {
+
+	
+
+	
+	private String id;
+	
+	
+	private String name;
+	
+	
+	private String email;
+	
+	private String photo;
+	
+	private String office;
+	
+	private String contact;
+	
+	private String cep;
+	
+	private String Street;
+	
+	private int numberHouse;
+	
+	private String state;
+	
+	private String Complement;
+	
+	private Enum Status;
+	
+	private Data CreateAt;
+	
+	private Data UpdateAt;
+	
+	
+	
+	
+	
+	
+	
+}
